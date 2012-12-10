fe2b4c638702ab4fe4f145ffe2e34d21207177d2
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2012-12-10 00:59:48 -0500

    update yml2md scripts

--- quiz5/data/yml2md.R
+++ quiz5/data/yml2md.R
@@ -19,3 +19,22 @@ qns = strsplit(slidify:::read_file(ymlFile), '\n---[^:.]*\n')[[1]]
 questions = lapply(qns, yml2md)
 cat(whisker.render(tpl), file = 'index.Rmd')
 q = lapply(lapply(qns, plyr::failwith(NULL, yaml::yaml.load, quiet = TRUE)), foo)
+
+#' Convert YAML file to list of questions
+#' 
+#' 
+yml2qns <- function(ymlFile){
+  yml2qn <- function(qn){
+    q = yaml::yaml.load(qn)
+    ans = gsub('^=(.*)$', '_\\1_', q$A, perl = TRUE)
+    num = seq_along(ans)
+    q$A = slidify:::zip_vectors(ans = ans, num = num)
+    return(q)
+  }
+  qns = strsplit(slidify:::read_file(ymlFile), '\n---[^:.]*\n')[[1]]
+  lapply(qns, plyr::failwith(NULL, yml2qn)) 
+}
+
+qns2md <- function(questions, tpl){
+  cat(whisker.render(tpl), file = 'index.Rmd')
+}
